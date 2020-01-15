35090f96466334b5ad0465584d2b002fe446f1e8
student <kishor.spd@gmail.com>
2020-01-15 21:15:13 +0530

    Lab1,2

---
+++ Lab2/input.c
@@ -0,0 +1,7 @@
+int main () {
+	char apple, banana, elephant;
+	int cat, dog;
+	cat = 5;
+
+	/*sdgsdgsfg*/
+}
---
+++ Lab2/test.c
@@ -0,0 +1,8 @@
+int main () {
+ char apple, banana, elephant;
+ int cat, dog;
+ cat = 5;
+
+ /*sdgsdgsfg*/
+}
+ÿ
